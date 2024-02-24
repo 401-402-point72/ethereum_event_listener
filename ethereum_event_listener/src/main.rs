1f85ac9bcec5a96200cc38e1bb7a8a1da17dbd79
Dean Longmire <deanlongmire100@gmail.com>
2024-02-24 14:08:36 -0500

    Transfer logic over from test_repo and modularize

--- ethereum_event_listener/src/main.rs
+++ ethereum_event_listener/src/main.rs
@@ -1,3 +1,29 @@
+mod s_3;
+mod web_3;
+
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Point72 Blockchain Menu: \n(1) listener\n(2) Store Data in S3");
+
+    let mut input = String::new();
+    match io::stdin().read_line(&mut input) {
+        Ok(_) => {
+            input = input.trim().to_string();
+
+            if input == "1" {
+                let _ = web_3::read_block_data();
+            } else if input == "2" {
+                if let Err(e) = s_3::main() {
+                    eprintln!("Error running S3 main: {:?}", e);
+                }
+            } else {
+                eprintln!("Error: choose 1 or 2 ");
+            }
+        }
+        Err(error) => {
+            // If an error occurred, print the error message
+            eprintln!("Error reading input: {}", error);
+        }
+    }
 }
